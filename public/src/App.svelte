cb58a8b63c8c36f16b2de44462ee4365d7775e9b
VitorGouveia <vitor.gouevia10@gmail.com>
2020-12-24 14:40:30 -0300

    svelte pwa template frontend pronto

--- public/src/App.svelte
+++ public/src/App.svelte
@@ -1,32 +1,44 @@
 <script>
-  const name = "pwa";
+ import Nav from './components/Nav.svelte'
+ import Header from './components/Header.svelte'
+ import Index from './components/Index.svelte'
 </script>
 
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
-
 <svelte:head>
-  <title>svelte pwa template</title>
+  <title>etec jrm pwa | Svelte</title>
 </svelte:head>
 
-<main>
-  <h1>hello {name}!</h1>
-  <img src="/assets/android-chrome-192x192.png" alt="svelte_icon" />
-</main>
+
+<Nav />
+<Header />
+<Index />
+
+    
+<style>
+    @import url("https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@700&display=swap");
+
+    :global(*) {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    :global(:root) {
+        --vermelho-etec: #820024;
+        --laranja-etec: #f68b37;
+        --neumorphism-shadow: -6px -6px 18px rgba(255, 255, 255, 0.3),
+            6px 6px 18px rgba(0, 0, 0, 0.16);
+        --dark-neumorphism-shadow: -5px -5px 10px rgba(255, 255, 255, 0.05),
+            5px 5px 15px rgba(0, 0, 0, 0.5);
+    }
+
+    :global(body) {
+        width: 100%;
+        height: 100%;
+        background: #dedede;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
---
+++ public/src/components/Nav.svelte
@@ -0,0 +1,214 @@
+<script>
+
+function showDropdown() {
+    const dropdown = document.querySelector(".dropdown");
+    if (dropdown.style.display == "flex") {
+        dropdown.style.display = "none";
+    } else {
+        dropdown.style.display = "flex";
+    }
+}
+
+function showDropdownDark() {
+    const dropdownDark = document.querySelector("#dark .dropdown");
+    if (dropdownDark.style.display == "flex") {
+        dropdownDark.style.display = "none";
+    } else {
+        dropdownDark.style.display = "flex";
+    }
+}
+
+</script>
+
+    <nav>
+        <header>
+            <h1>INÍCIO</h1>
+            <svg on:click = { showDropdown } class="hamburger" 
+                xmlns="http://www.w3.org/2000/svg" 
+                width="40" 
+                height="40" 
+                viewBox="0 0 24 24" 
+                fill="none" 
+                stroke="currentColor" 
+                stroke-width="3" 
+                stroke-linecap="round" 
+                stroke-linejoin="round">
+                <line x1="3" y1="12" x2="21" y2="12"></line>
+                <line x1="3" y1="6" x2="21" y2="6"></line>
+                <line x1="3" y1="18" x2="21" y2="18"></line>
+            </svg>
+        </header>
+
+        <div class="dropdown">
+            <div class="row">
+                <h1>PROPÓSITO</h1>
+            </div>
+            <div class="row">
+                <h1>ESPAÇO DO ALUNO</h1>
+            </div>
+            <div class="row">
+                <h1>CURSOS TÉCNICOS</h1>
+            </div>
+            <div class="row">
+                <h1>FALE CONOSCO</h1>
+            </div>
+        </div>
+    </nav>
+    <!-- <nav id="dark">
+        <header>
+            <h1>INÍCIO</h1>
+            <svg on:click = { showDropdownDark } class="hamburger" 
+                xmlns="http://www.w3.org/2000/svg" 
+                width="40" 
+                height="40" 
+                viewBox="0 0 24 24" 
+                fill="none" 
+                stroke="currentColor" 
+                stroke-width="3" 
+                stroke-linecap="round" 
+                stroke-linejoin="round">
+                <line x1="3" y1="12" x2="21" y2="12"></line>
+                <line x1="3" y1="6" x2="21" y2="6"></line>
+                <line x1="3" y1="18" x2="21" y2="18"></line>
+            </svg>
+        </header>
+
+        <div class="dropdown">
+            <div class="row">
+                <h1>PROPÓSITO</h1>
+            </div>
+            <div class="row">
+                <h1>ESPAÇO DO ALUNO</h1>
+            </div>
+            <div class="row">
+                <h1>CURSOS TÉCNICOS</h1>
+            </div>
+            <div class="row">
+                <h1>FALE CONOSCO</h1>
+            </div>
+        </div>
+    </nav> -->
+
+<style> 
+    nav {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        width: 80%;
+        border-radius: 12px;
+        padding: 2%;
+        margin: 2% 0;
+        align-items: center;
+        background: #fff;
+        box-shadow: var(--neumorphism-shadow);
+    }
+
+    nav header {
+        display: flex;
+        width: 95%;
+        border-radius: 12px;
+        padding: 2% 6%;
+        align-items: center;
+        justify-content: space-between;
+        flex-direction: row;
+        box-shadow: inset -6px -6px 18px #ffffff, inset 6px 6px 18px rgba(0, 0, 0, 0.16);
+    }
+
+    nav h1 {
+        color: var(--vermelho-etec);
+        font-size: 3vmax;
+        font-family: "Roboto Condensed", sans-serif;
+        font-weight: bold;
+    }
+
+    nav .hamburger {
+        cursor: pointer;
+        width: 4vmax;
+        color: var(--vermelho-etec);
+    }
+
+    nav .dropdown {
+        display: none;
+        width: 100%;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    nav .dropdown .row {
+        display: flex;
+        cursor: pointer;
+        flex-direction: row;
+        align-items: center;
+        margin: 1% 0;
+        padding: 2% 6%;
+        width: 95%;
+        border-radius: 12px;
+        box-shadow: var(--neumorphism-shadow);
+    }
+
+    nav .dropdown .row:active {
+        cursor: pointer;
+        box-shadow: inset -6px -6px 18px #ffffff, inset 6px 6px 18px rgba(0, 0, 0, 0.16);
+    }
+
+    nav#dark {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        width: 80%;
+        border-radius: 12px;
+        padding: 2%;
+        margin: 2% 0;
+        align-items: center;
+        background: #1b1b1b;
+        box-shadow: var(--dark-neumorphism-shadow);
+    }
+
+    nav#dark header {
+        display: flex;
+        width: 95%;
+        border-radius: 12px;
+        padding: 2% 6%;
+        align-items: center;
+        justify-content: space-between;
+        flex-direction: row;
+        box-shadow: inset -5px -5px 10px rgba(255, 255, 255, 0.05), inset 5px 5px 15px rgba(0, 0, 0, 0.5);
+    }
+
+    nav#dark h1 {
+        color: var(--laranja-etec);
+        font-size: 3vmax;
+        font-family: "Roboto Condensed", sans-serif;
+        font-weight: bold;
+    }
+
+    nav#dark .hamburger {
+        cursor: pointer;
+        width: 4vmax;
+        color: var(--laranja-etec);
+    }
+
+    nav#dark .dropdown {
+        display: none;
+        width: 100%;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    nav#dark .dropdown .row {
+        display: flex;
+        cursor: pointer;
+        flex-direction: row;
+        align-items: center;
+        margin: 1% 0;
+        padding: 2% 6%;
+        width: 95%;
+        border-radius: 12px;
+        box-shadow: var(--dark-neumorphism-shadow);
+    }
+
+    nav#dark .dropdown .row:active {
+        cursor: pointer;
+        box-shadow: inset -5px -5px 10px rgba(255, 255, 255, 0.05), inset 5px 5px 15px rgba(0, 0, 0, 0.5);
+    }
+</style>
